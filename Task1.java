86bbda933484489ffb07760de1150456f1e92029
asma245 <62486425+asma245@users.noreply.github.com>
2020-03-22 01:35:43 +0200

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,128 @@
+public class first {
+    public static void main (String [] args)
+    {
+        int a= -5+8*6;
+       int b= (55+9)%9;
+      int  c=20 + -3*5 / 8;
+       int d= 5 + 15 / 3* 2-8%3;
+        System.out.println("a:"+a);
+         System.out.println("b:"+b);
+          System.out.println("c:"+c);
+           System.out.println("d:"+d);
+    }
+}
+
+
+
+import java.util.Scanner;
+
+public class Second {
+     public static void main(String[] args)
+    {
+
+    Scanner a =new Scanner(System.in);
+System.out.println("enter the two numbers");
+	int num1=a.nextInt();
+	int num2=a.nextInt();
+    int sum = num1+num2;
+    int diff= num1-num2;
+    int multiply=num1*num2;
+   float subtract= num1/num2;
+   float  remainder  = num1  %num2;
+  System.out.println("Sum="+ sum);
+   System.out.println("Difference="+ diff);
+    System.out.println("Multiply="+ multiply);
+     System.out.println("Subtract="+ subtract);
+      System.out.println("Remainder of two numbers="+ remainder);
+    }
+   
+}
+
+
+
+
+
+import java .util.Scanner;
+
+public class Three {
+     public static void main(String[] args)
+    {
+          Scanner x = new Scanner (System.in);
+int num;
+        System.out.println("Please, Enter the  number");
+   num=x.nextInt();
+   int multi;
+   for (int i=1;i<=10;i++)
+   {
+  multi  =num *i;
+   System.out.println("multi "+i+" = "+multi);
+   }
+
+
+    }
+    
+}
+
+
+public class Four {
+    public static void main(String[] args)
+    {
+ System.out.println("   J    a   v     v  a   ");
+System.out.println("   J   a a   v   v  a a  ");
+System.out.println("J  J  aaaaa   V V  aaaaa ");
+System.out.println(" JJ  a     a   V  a     a ");
+
+    }
+
+}
+
+
+public class Five {
+    public static void main(String[] args)
+    { double a=((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    System.out.println("a="+a);
+    }
+    
+}
+
+
+
+public class Six {
+      public static void main(String[] args)
+    { double a=4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+    System.out.println("a="+a);
+    }
+}
+
+public class Seven {
+    public static void main(String[] args)
+    {
+       double r= 7.5;
+        double Area =3.14*r*r;
+        double  perimeter =2*3.14*r;
+        System.out.println("Area:"+Area);
+         System.out.println(" Perimeter:"+ perimeter);
+    }
+    
+}
+
+
+import java.util.Scanner;
+
+public class Seven {
+    public static void main(String[] args)
+    {Scanner a=new Scanner(System.in);
+System.out.println("Enter the Raduis");
+double rad=a.nextFloat();
+       double r= 7.5;
+        double Area =3.14*r*r;
+        double  perimeter =2*3.14*r;
+        System.out.println("Area:"+Area);
+         System.out.println(" Perimeter:"+ perimeter);
+    }
+    
+}
+
+
+
+
